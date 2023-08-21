5eae3aab34627d73b9dd712db23392a6dc6f8dac
Ahmedbabani <ahmedahmedibrahim351@gmail.com>
2023-08-22 00:54:30 +0300

    restful api

---
+++ rest_api/lib/controlers/user_controller.dart
@@ -0,0 +1,109 @@
+import 'dart:convert';
+
+import 'package:rest_api/models/user_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class UserController extends GetxController {
+  UserModel? userModel;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController genderController = TextEditingController();
+  TextEditingController statusController = TextEditingController();
+
+  fetchUserData(String userid) async {
+    
+    var token =
+        'tokene xot daxl bka';
+    try {
+      var headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token'
+      };
+      http.Response response = await http.get(
+          Uri.tryParse('https://gorest.co.in/public/v2/users/$userid')!,
+          headers: headers);
+      if (response.statusCode == 200) {
+        var result = jsonDecode(response.body);
+        print(result);
+        userModel = UserModel.fromJson(result);
+        nameController.text = result['name'];
+        emailController.text = result['email'];
+        genderController.text = result['gender'];
+        statusController.text = result['status'];
+       
+      } else {
+        print('error fetching data');
+      }
+    } catch (e) {
+      print('error $e');
+    }
+  }
+
+  updateUserData(String userid) async {
+   
+    var token =
+        'tokene xot daxl bka';
+    try {
+      var body = {
+        'name': nameController.text,
+        'email': emailController.text,
+        'gender': genderController.text,
+        'status': statusController.text,
+      };
+
+      http.Response response = await http.put(
+        Uri.parse('https://gorest.co.in/public/v2/users/$userid'),
+        body: jsonEncode(body),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        print('data updated');
+        print(json);
+      } else {
+        print('response status code not 200');
+        throw jsonDecode(response.body)['meta']["message"] ??
+            "Unknown Error Occured";
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  deleteUserData(String userid) async {
+    //getx delete api data
+    var token =
+        'tokene xot daxl bka';
+    try {
+      http.Response response = await http.delete(
+        Uri.parse('https://gorest.co.in/public/v2/users/$userid'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        // final json = jsonDecode(response.body);
+        // delete success, do anything
+        print('deleted user $userid');
+      } else {
+        print('response status code not 200');
+        throw jsonDecode(response.body)['meta']["message"] ??
+            "Unknown Error Occured";
+      }
+      //4494636
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- rest_api/lib/main.dart
+++ rest_api/lib/main.dart
@@ -1,84 +1,91 @@
-import 'dart:convert';
-
+import 'controlers/user_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:rest_api/profile.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(Myapp());
+main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class Myapp extends StatefulWidget {
-  @override
-  State<Myapp> createState() => _MyappState();
-}
-
-class _MyappState extends State<Myapp> {
-  Future getData() async {
-    var url = Uri.parse('https://jsonplaceholder.typicode.com/users');
-    var response = await http.get(url);
-    return jsonDecode(response.body);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    // getData();
-  }
+class MyApp extends StatelessWidget {
+  UserController userController = Get.put(UserController());
+  TextEditingController userIdController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white60,
-            foregroundColor: Colors.black,
-            title: Text('Rest Api'),
-            centerTitle: true,
-          ),
-          body: FutureBuilder(
-            future: getData(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ListTile(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            tileColor: Colors.black12,
-                            onTap: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Profile(),
-                                )),
-                            leading: CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg'),
-                            ),
-                            subtitle: Text(snapshot.data[index]['email']),
-                            title: Text(snapshot.data[index]['name']),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              } else {
-                return Center(child: Text('No data available'));
-              }
+    return Scaffold(
+      appBar: AppBar(title: Text('API Update & Delete'), actions: [
+        TextButton(
+            onPressed: () {
+              userController.nameController.clear();
+              userController.emailController.clear();
+              userController.genderController.clear();
+              userController.statusController.clear();
             },
+            child: Text(
+              'clear',
+              style: TextStyle(color: Colors.white),
+            ))
+      ]),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: ListView(children: [
+          TextField(
+            controller: userController.nameController,
+            decoration: InputDecoration(hintText: 'name'),
           ),
-        ));
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: userController.emailController,
+            decoration: InputDecoration(hintText: 'email'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: userController.genderController,
+            decoration: InputDecoration(hintText: 'gender'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: userController.statusController,
+            decoration: InputDecoration(hintText: 'status'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: userIdController,
+            decoration: InputDecoration(hintText: 'user id'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              userController.fetchUserData(userIdController.text);
+            },
+            child: Text('get data'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              userController.updateUserData(userIdController.text);
+            },
+            child: Text('update data'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              userController.deleteUserData(userIdController.text);
+            },
+            child: Text('delete data'),
+          )
+        ]),
+      ),
+    );
   }
 }
